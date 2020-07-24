15ae14022bdadcb972b1e65dedc5cf97169bd1e9
Balasubramanian N <nsbala@thoughtworks.com>
2020-07-24 19:44:52 +0530

    Installed the dependencies in airflow machine itself. So build and packaging is not required.

--- StationMartChecker/deploy.sh
+++ StationMartChecker/deploy.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-echo "====Packaging===="
-virtualenv station_mart_checker
-mkdir station_mart_checker_contents && cd station_mart_checker_contents
-
-pip install --install-option="--install-lib=$PWD" hdfs
-pip install --install-option="--install-lib=$PWD" pytz
-echo `pwd`
-cp ../StationMartChecker/dags/* ./
-zip -r station-mart-checker.zip ./*
-
-echo "====Copy zip to airflow server===="
-scp ./station-mart-checker.zip airflow.${TRAINING_COHORT}.training:/home/ec2-user/airflow/dags
-echo "====Zip copied to airflow server===="
+echo "====Copy dags to airflow server===="
+scp ./StationMartChecker/dags/station-mart-checker.py airflow.${TRAINING_COHORT}.training:/home/ec2-user/airflow/dags
+echo "====Dags copied to airflow server===="
