dfa40df9d43aecc4a5545da0f13d6f20dc71c6f8
Balasubramanian N <nsbala@thoughtworks.com>
2020-07-25 13:33:14 +0530

    Added environment information in SNS email message

--- StationMartChecker/dags/station-mart-checker.py
+++ StationMartChecker/dags/station-mart-checker.py
@@ -25,18 +25,18 @@ utc = pytz.timezone("UTC")
 hdfs_server_endpoint = Variable.get('hdfs_server_endpoint')
 csv_file_path = Variable.get('csv_file_path')
 topicArn = Variable.get('sns_csv_publisher_topic_arn')
-
+env = Variable.get('env')
 
 def send_sns_notification():
 
-    message = 'CSV UPDATE ERROR\n\nIt seems like the %s file was last updated more than %d seconds ago' % (csv_file_path, file_alive_threshold)
+    message = 'CSV UPDATE ERROR - %s \n\nIt seems like the %s file was last updated more than %d seconds ago' % (env, csv_file_path, file_alive_threshold)
     sns_client = aws_sns_hook.AwsSnsHook(aws_conn_id='aws_sns_connection')
     response = sns_client.publish_to_target(topicArn, message)
     print(response)
 
 
 def check_for_fresh_file(ds, **kwargs):
-    print('Checking whether the file %s exists in hdfs server' % csv_file_path)
+    print('Checking whether the file %s exists in %s-hdfs server' % (csv_file_path, env))
 
     client = InsecureClient(hdfs_server_endpoint, user='root')
     csv_meta_data = client.status(csv_file_path)
