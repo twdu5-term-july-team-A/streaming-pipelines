31425b0723f5c1c751df76728d00f38193c52e10
Gobinath Manokaran <>
2020-07-29 15:47:14 +0530

    Fixed the issue in kill command

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -71,14 +71,14 @@ ssh ingester${HOST_SUFFIX}.${TRAINING_COHORT}.training <<EOF
 set -e
 
 function kill_process {
-    query=\$1
-    pid=`ps aux | grep \$query | grep -v "grep" |  awk "{print \\\$2}"`
+    query=$1
+    pid=`ps -aef | grep $query | grep -v sh| grep -v grep| awk '{print $2}'`
 
-    if [ -z "\$pid" ];
+    if [ -z "$pid" ];
     then
         echo "no \${query} process running"
     else
-        kill -9 \$pid
+        kill -9 $pid
     fi
 }
 
@@ -89,10 +89,10 @@ station_france="station-france"
 
 echo "====Kill running producers===="
 
-kill_process \${station_information}
-kill_process \${station_status}
-kill_process \${station_san_francisco}
-kill_process \${station_france}
+kill_process ${station_information}
+kill_process ${station_status}
+kill_process ${station_san_francisco}
+kill_process ${station_france}
 
 echo "====Runing Producers Killed===="
 
