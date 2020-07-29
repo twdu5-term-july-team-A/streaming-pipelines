fdfc0c4ca390b1b2633bf6eac50128058835d17b
Biao Tian <btian@thoughtworks.com>
2020-07-29 17:36:59 +0800

    get test result when running the integration test

--- sbin/runE2ETest.sh
+++ sbin/runE2ETest.sh
@@ -7,4 +7,22 @@ $DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
 echo "====Building Consumer JARs===="
 cd $DIR/../StationConsumer && sbt package
 echo "====Running docker-compose===="
-$DIR/../docker/docker-compose.sh --project-directory $DIR/../docker -f $DIR/../docker/docker-compose-test.yml up --build
+$DIR/../docker/docker-compose.sh --project-directory $DIR/../docker -f $DIR/../docker/docker-compose-test.yml up --build -d
+
+docker build -t e2e-test $DIR/../E2ETest
+
+docker run \
+  --network=streamingdatapipeline_streaming-data-internal \
+  -v ~/.ivy2:/root/.ivy2:rw \
+  -it  e2e-test \
+  /bin/bash -c "cd /E2ETest && sbt test"
+
+if [ $? -ne 0 ]
+then
+  echo "Test Failed"
+  exit 255
+else
+  echo "Test Passed"
+fi
+
+
