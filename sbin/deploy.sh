c0d5ef9f0716666f08319ef5f106619d4f339369
Balasubramanian N <nsbala@thoughtworks.com>
2020-07-30 15:59:50 +0530

    Multinode kafka server changes

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -21,7 +21,10 @@ if [ ${ENV} != "prod" ]; then
 fi
 
 EMR_MASTER_HOST=emr-master${HOST_SUFFIX}.${TRAINING_COHORT}.training
-KAFKA_HOST=kafka${HOST_SUFFIX}.${TRAINING_COHORT}.training
+
+KAFKA_HOST1=kafka${HOST_SUFFIX}1.${TRAINING_COHORT}.training
+KAFKA_HOST2=kafka${HOST_SUFFIX}2.${TRAINING_COHORT}.training
+KAFKA_HOST3=kafka${HOST_SUFFIX}3.${TRAINING_COHORT}.training
 
 echo "====Updating SSH Config===="
 
@@ -52,11 +55,11 @@ Host bastion.${TRAINING_COHORT}.training
 echo "====SSH Config Updated===="
 
 echo "====Insert app config in zookeeper===="
-scp ./zookeeper/seed.sh ${KAFKA_HOST}:/tmp/zookeeper-seed.sh
-ssh ${KAFKA_HOST} <<EOF
+scp ./zookeeper/seed.sh ${KAFKA_HOST1}:/tmp/zookeeper-seed.sh
+ssh ${KAFKA_HOST1} <<EOF
 set -e
 export hdfs_server="${EMR_MASTER_HOST}:8020"
-export kafka_server="${KAFKA_HOST}:9092"
+export kafka_server="${KAFKA_HOST1}:9092"
 export zk_command="zookeeper-shell localhost:2181"
 sh /tmp/zookeeper-seed.sh
 EOF
@@ -84,10 +87,10 @@ station_san_francisco="station-san-francisco"
 station_marseille="station-marseille"
 
 echo "====Deploy Producers===="
-nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_information} --kafka.brokers=${KAFKA_HOST}:9092 1>/tmp/\${station_information}.log 2>/tmp/\${station_information}.error.log &
-nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_san_francisco} --producer.topic=station_data_sf --kafka.brokers=${KAFKA_HOST}:9092 1>/tmp/\${station_san_francisco}.log 2>/tmp/\${station_san_francisco}.error.log &
-nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_status} --kafka.brokers=${KAFKA_HOST}:9092 1>/tmp/\${station_status}.log 2>/tmp/\${station_status}.error.log &
-nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_marseille} --producer.topic=station_data_marseille --kafka.brokers=${KAFKA_HOST}:9092 1>/tmp/\${station_marseille}.log 2>/tmp/\${station_marseille}.error.log &
+nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_information} --kafka.brokers=${KAFKA_HOST1}:9092,${KAFKA_HOST2}:9092,${KAFKA_HOST3}:9092 1>/tmp/\${station_information}.log 2>/tmp/\${station_information}.error.log &
+nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_san_francisco} --producer.topic=station_data_sf --kafka.brokers=${KAFKA_HOST1}:9092,${KAFKA_HOST2}:9092,${KAFKA_HOST3}:9092 1>/tmp/\${station_san_francisco}.log 2>/tmp/\${station_san_francisco}.error.log &
+nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_status} --kafka.brokers=${KAFKA_HOST1}:9092,${KAFKA_HOST2}:9092,${KAFKA_HOST3}:9092 1>/tmp/\${station_status}.log 2>/tmp/\${station_status}.error.log &
+nohup java -jar /tmp/tw-citibike-apis-producer0.1.0.jar --spring.profiles.active=\${station_marseille} --producer.topic=station_data_marseille --kafka.brokers=${KAFKA_HOST1}:9092,${KAFKA_HOST2}:9092,${KAFKA_HOST3}:9092 1>/tmp/\${station_marseille}.log 2>/tmp/\${station_marseille}.error.log &
 
 echo "====Producers Deployed===="
 EOF
@@ -127,11 +130,11 @@ echo "====Old Raw Data Saver Killed===="
 
 echo "====Deploy Raw Data Saver===="
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationStatusSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --executor-memory 1g --executor-cores 1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar ${KAFKA_HOST}:2181 "/tw/stationStatus" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationStatusSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --executor-memory 1g --executor-cores 1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar ${KAFKA_HOST1}:2181,${KAFKA_HOST2}:2181,${KAFKA_HOST3}:2181 "/tw/stationStatus" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationInformationSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --executor-memory 1g --executor-cores 1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar ${KAFKA_HOST}:2181 "/tw/stationInformation" 1>/tmp/raw-station-information-data-saver.log 2>/tmp/raw-station-information-data-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationInformationSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --executor-memory 1g --executor-cores 1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar ${KAFKA_HOST1}:2181,${KAFKA_HOST2}:2181,${KAFKA_HOST3}:2181 "/tw/stationInformation" 1>/tmp/raw-station-information-data-saver.log 2>/tmp/raw-station-information-data-saver.error.log &
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --executor-memory 1g --executor-cores 1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar ${KAFKA_HOST}:2181 "/tw/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --executor-memory 1g --executor-cores 1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar ${KAFKA_HOST1}:2181,${KAFKA_HOST2}:2181,${KAFKA_HOST3}:2181 "/tw/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
 
 echo "====Raw Data Saver Deployed===="
 EOF
@@ -160,9 +163,9 @@ echo "====Old Station Consumers Killed===="
 
 echo "====Deploy Station Consumers===="
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationApp --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --num-executors 2 --driver-memory 500M --executor-memory 2g --executor-cores 2 /tmp/tw-station-consumer_2.11-0.0.1.jar ${KAFKA_HOST}:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationApp --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --num-executors 2 --driver-memory 500M --executor-memory 2g --executor-cores 2 /tmp/tw-station-consumer_2.11-0.0.1.jar ${KAFKA_HOST1}:2181,${KAFKA_HOST2}:2181,${KAFKA_HOST3}:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationApp --name StationTransformerNYC --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --num-executors 2 --driver-memory 500M --executor-memory 2g --executor-cores 2 /tmp/tw-station-transformer-nyc_2.11-0.0.1.jar ${KAFKA_HOST}:2181 1>/tmp/station-transformer-nyc.log 2>/tmp/station-transformer-nyc.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationApp --name StationTransformerNYC --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0  --num-executors 2 --driver-memory 500M --executor-memory 2g --executor-cores 2 /tmp/tw-station-transformer-nyc_2.11-0.0.1.jar ${KAFKA_HOST1}:2181,${KAFKA_HOST2}:2181,${KAFKA_HOST3}:2181 1>/tmp/station-transformer-nyc.log 2>/tmp/station-transformer-nyc.error.log &
 
 echo "====Station Consumers Deployed===="
 EOF
